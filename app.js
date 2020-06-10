b6e54357445a345b6d98c5d50ba37c922b675a17
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-11 02:22:02 +0530

    Testing the Library ..

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var g = G$('firstname', 'lastname');
 g.greet().setLanguage('am').greet(true);
+
+// let's use our object on the click of the login button
+$('#login').click(function() {
+   
+    // create a new 'Greetr' object (let's pretend we know the name from the login)
+    var loginGrtr = G$('Fitsum', 'Alemu');
+    
+     // hide the login on the screen
+    $('#logindiv').hide();
+    
+     // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log the welcome as well
+    loginGrtr.setLanguage($('#lang').val()). HTMLGreeting('#greeting', true).log();
+    
+});
